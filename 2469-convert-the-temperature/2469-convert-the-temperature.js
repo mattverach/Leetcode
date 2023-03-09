b5277537d98011bacc693289886e09956e5565ab
Matías Vera <85206468+mattverach@users.noreply.github.com>
2023-03-09 00:05:04 -0300

    Time: 67 ms (37.83%), Space: 41.6 MB (81.75%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} celsius
+ * @return {number[]}
+ */
+var convertTemperature = function(celsius) {
+
+    const output = new Array();
+
+    output.push(celsius + 273.15, fahrenheit = celsius * 1.80 + 32.00);
+
+    return output;
+
+    
+
+    
+};
